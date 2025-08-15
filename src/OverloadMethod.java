e73d1dec0bf72af4b8c9585f863f922d9a98cb49
Sailesh T <saileshtrn06@gmail.com>
2025-08-15 10:39:50 +0530

    program added

--- src/OverloadMethod.java
+++ src/OverloadMethod.java
@@ -1,5 +1,17 @@
 public class OverloadMethod {
     public static void main(String [] args){
+        System.out.println(add(1,2 ,3, 4 ,5));
+    }
 
+    static double add(double a , double b){
+        return a + b;
+    }
+
+    static double add(double a, double b , double c , double d){
+        return a + b + c + d;
+    }
+
+    static double add(double a , double b , double c , double d , double e){
+        return a + b - c * d + e;
     }
 }
