b27ce32aaad6a59533c323fefaebce223e9489f7
Sailesh T <saileshtrn06@gmail.com>
2025-08-21 19:29:14 +0530

    program added

--- src/Loops.java
+++ src/Loops.java
@@ -23,10 +23,10 @@ public class Loops {
         else{
             System.out.println("FAIL!");
         }
-        int i;
 
-        for(i = 0 ; i < 4 ; i++){
-            System.out.println(i+" ");
+
+        for(int j = 0 ; j < 4 ; j++){
+            System.out.println(j+" ");
         }
     }
 }
